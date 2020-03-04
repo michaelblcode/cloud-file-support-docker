adfc947e5be330fca5353540bfb7ec1add626095
michaelblcode <michael.benares2020@gmail.com>
2020-03-04 18:13:39 -0500

    Update crontab and finish rclone sync

--- rclone/rclone.sh
+++ rclone/rclone.sh
@@ -8,54 +8,66 @@ source /cron/rclone.env
 function rclone_copy() {
   (
     flock -n 200 || exit 1
-    sync_command="rclone copy ${RCLONE_CROND_SOURCE_PATH} ${RCLONE_CROND_DESTINATION_PATH}"
 
-    if [ "${RCLONE_SYNC_COMMAND}" ]; then
-      sync_command="${RCLONE_SYNC_COMMAND}"
-    else
-      if [[ -z "${RCLONE_CROND_SOURCE_PATH}" ]] || [[ -z "${RCLONE_CROND_DESTINATION_PATH}" ]]; then
-        echo "Error: A RCLONE PATH environment variable was not set or passed to the container. Please review your RCLONE source/destination paths."
+    sync_list=/config/rclone/sync_list.conf
+    while IFS= read -r line; do
+      IFS=','
+      read -ra path <<<"$line"
+
+      src=${path[0]}
+      dst="${RCLONE_RSYNC_PATH}${path[1]}"
+
+      sync_command="rclone copy $src $dst"
+
+      if [[ -z "$src" ]] || [[ -z "$dst" ]]; then
+        echo "Error: src and/or dst are/is empty. Please review your sync_list.conf file."
         exit 1
       fi
-    fi
 
-    echo "Executing => ${sync_command}"
-    eval "${sync_command}" || send
+      echo "Excuting - $sync_command"
+      eval "$sync_command"
+    done <$sync_list
   ) 200>/run/rclone.lock
 }
 
 function rclone_move() {
   (
     flock -n 200 || exit 1
-    sync_command="rclone move ${RCLONE_CROND_SOURCE_PATH} ${RCLONE_CROND_DESTINATION_PATH}"
-    if [ "${RCLONE_SYNC_COMMAND}" ]; then
-      sync_command="${RCLONE_SYNC_COMMAND}"
-    else
-      if [[ -z "${RCLONE_CROND_SOURCE_PATH}" ]] || [[ -z "${RCLONE_CROND_DESTINATION_PATH}" ]]; then
-        echo "Error: A RCLONE PATH environment variable was not set or passed to the container. Please review your RCLONE source/destination paths."
-        exit 1
-      fi
+
+    sync_command="rclone copy $1 $2"
+
+    if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+      echo "Error: src and/or dst are/is empty. Please review your sync_list.conf file."
+      exit 1
     fi
-    echo "Executing => ${sync_command}"
-    eval "${sync_command}" || send
+
+    echo "Excuting - $sync_command"
+    eval "$sync_command"
   ) 200>/run/rclone.lock
 }
 
 function foldersize() {
-  if [[ -z "${RCLONE_CROND_SOURCE_PATH}" ]] || [[ -z "${RCLONE_CROND_SOURCE_SIZE}" ]]; then
-    echo "INFO: A local folder path and/or size has not been set. Not using folder monitor"
-  else
-    SIZE=$(/usr/bin/du -s ${RCLONE_CROND_SOURCE_PATH} | /usr/bin/awk '{print $1}')
-    MBSIZE=$((SIZE / 1024))
-    echo "${RCLONE_CROND_SOURCE_PATH} is $MBSIZE MB"
-    if [[ ${MBSIZE} -gt $((${RCLONE_CROND_SOURCE_SIZE})) ]]; then
-      rclone_move
+  sync_list=/config/rclone/sync_list.conf
+  while IFS= read -r line; do
+    IFS=','
+    read -ra path <<<"$line"
+    src=${path[0]}
+    dst="${RCLONE_RSYNC_PATH}${path[1]}"
+
+    if [[ -z "${src}" ]]; then
+      echo "INFO: source folder hasn't been set"
+    else
+      SIZE=$(/usr/bin/du -s ${src} | /usr/bin/awk '{print $1}')
+      MBSIZE=$((SIZE / 1024))
+      echo "${src} is $MBSIZE MB"
+      if [[ ${MBSIZE} -gt $((${RCLONE_CROND_SOURCE_SIZE})) ]]; then
+        rclone_move "${src}" "${dst}"
+      fi
     fi
-  fi
+  done <$sync_list
 }
 
 function run() {
-  echo "sfsfsfs"
   rclone_copy
   foldersize
 }
